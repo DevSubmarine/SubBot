173b4675da6c309b414fbf8e1a70a8e87c726fb6
Ibrahim Hisham <minidevz@users.noreply.github.com>
2020-09-23 18:47:11 +0500

    Added avatar.js

--- commands/about.js
+++ commands/about.js
@@ -2,16 +2,16 @@ const Discord = require('discord.js');
 
 module.exports.config = { 
     name: "about",
-    aliases: ["info"]
+    aliases: ["info", "abt"]
 }
 
 module.exports.run = async (client, message, args) => {
-    const aboutEmbed = new Discord.MessageEmbed()
-    aboutEmbed.setTitle(message.author.username)
-    aboutEmbed.setColor(3066993)   
-    aboutEmbed.setAuthor("Your Avatar!")
-    aboutEmbed.setImage(message.author.displayAvatarURL());
-    message.channel.send(avatarEmbed);
-
-       
+    const exampleEmbed = new Discord.MessageEmbed()
+        .setColor('#ff9841')
+        .setTitle('What is SubBot?')
+        .setDescription('SubBot is the official DevSubmarine Discord Bot! It features lots of funny and silly commands for the Dev Submarine server! If you would like to contribute or check out the repo for this bot, click [here](https://github.com/DevSubmarine/SubBot)!')
+        .setThumbnail("https://cdn.discordapp.com/avatars/757989961512386731/2fd8d7f3248e48d63ded67c9536556bf.webp")
+        .setFooter("Credits: Guray, TehGM, harsh-dev and IbrahimDev.");
+    
+    message.channel.send(exampleEmbed);
 }
